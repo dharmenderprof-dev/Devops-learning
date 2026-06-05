44f0218517b1ba0b17042c6f86f97e986d597121
dharmenderprof-dev <dharmender.prof@gmail.com>
2026-06-05 16:20:16 +0530

    changes in database file

--- projects/employee-review-system/database/db.js
+++ projects/employee-review-system/database/db.js
@@ -1,11 +1,11 @@
 const { Pool } = require('pg');
 
 const pool = new Pool({
-    user: 'postgres',
-    host: 'db',
-    database: 'employeeportal',
-    password: 'Admin123!',
-    port: 5432,
+    user: process.env.DB_USER,
+    host: process.env.DB_HOST,
+    database: process.env.DB_NAME,
+    password: process.env.DB_PASSWORD,
+    port: process.env.DB_PORT,
 });
 
 module.exports = pool;
